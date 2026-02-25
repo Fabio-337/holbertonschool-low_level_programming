41667e8d42ebec525b1cc82ed32ab3ba35abd07c
fabio <13018@holbertonstudents.com>
2026-02-25 10:36:13 +0000

    task5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');  // affiche '+'
+		return (1);     // retourne 1
+	}
+	else if (n == 0)
+	{
+		_putchar('0');  // affiche '0'
+		return (0);     // retourne 0
+	}
+	else
+	{
+		_putchar('-');  // affiche '-'
+		return (-1);    // retourne -1
+	}
+}
