1196b428579d583645d470487842dfa20e688c78
fabio <13018@holbertonstudents.com>
2026-02-26 14:45:03 +0000

    task 1ok

--- intro_debugging/sum_to_n.c
+++ intro_debugging/sum_to_n.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * sum_to_n - computes the sum of all integers from 1 to n
  * @n: the maximum number to add
@@ -15,4 +17,12 @@ int sum_to_n(int n)
     }
 
     return (sum);
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+    printf("%d\n", sum_to_n(10));
+    return (0);
 }
